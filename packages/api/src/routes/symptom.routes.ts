09be6f03a82ff9f7bcc496279919c01c3fd84dee
Rami Nouehed <127147493+rm12000356@users.noreply.github.com>
2026-07-10 16:21:04 +0300

    Refactor: import paths and format route definition

--- packages/api/src/routes/symptom.routes.ts
+++ packages/api/src/routes/symptom.routes.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import { symptomCatalogController } from "../controllers/symptom.controller";
-import { rateLimiter } from "src/middleware/rate-limiter";
+import { rateLimiter } from "../middleware/rate-limiter";
 import { SymptomQuerySchema } from "../schemas/symptoms.schemas";
-import {validate} from "src/middleware/validate";
-
-
+import {validate} from "../middleware/validate";
 
 const router = Router();
 
-router.get("/symptoms", rateLimiter,validate(SymptomQuerySchema, "query"), symptomCatalogController.list);
+router.get("/symptoms", 
+           rateLimiter,
+           validate(SymptomQuerySchema, "query"),
+           symptomCatalogController.list
+          );
 
 
 export { router as symptomCatalogRouter };
